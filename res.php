de5a5a06e744945549c75c480e331e924ce0309b
Jose Carlos Limpiada <84702564+201910077@users.noreply.github.com>
2022-02-22 17:28:06 +0800

    Add files via upload

---
+++ res.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html PUBLIC >
+<?php
+$RESUME = "MY RESUME";?>
+<html>
+<head>
+
+	<title>Jose Carlos R. Limpiada | Student, First Year | 201910077@feualabang.edu.ph</title>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+
+	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.7.0/build/reset-fonts-grids/reset-fonts-grids.css" media="all" /> 
+	<link rel="stylesheet" type="text/css" href="resume.css" media="all" />
+</head>
+<body>
+<?php 
+echo $RESUME;
+?><br/>
+<div id="doc2" class="yui-t7">
+	<div id="inner">
+	
+		<div id="hd">
+			<div class="yui-gc">
+				<div class="yui-u first">
+					<h1>Jose Carlos R. Limpiada</h1>
+					<h2>Student, Third Year</h2>
+				</div>
+
+				<div class="yui-u">
+					<div class="contact-info">
+						<h3><a id="pdf" href="#">Learn More</a></h3>
+						<h3><a href="">201910077@feualabang.edu.ph</a></h3>
+						<h3>(123) - 456 - 789</h3>
+					</div><!--// .contact-info -->
+				</div>
+			</div><!--// .yui-gc -->
+		</div><!--// hd -->
+
+		<div id="bd">
+			<div id="yui-main">
+				<div class="yui-b">
+
+					<div class="yui-gf">
+						<div class="yui-u first">
+							<h2>Profile</h2>
+						</div>
+						<div class="yui-u">
+							<p class="enlarge">
+								Competitive, and smart prodigy in the field of analytics and logic. His critical-thinking  skills make him a good decision maker not just in the academic sense but also in his day-to-day life.
+							</p>
+						</div>
+					</div><!--// .yui-gf -->
+
+					<div class="yui-gf">
+						<div class="yui-u first">
+							<h2>Skills</h2>
+						</div>
+						<div class="yui-u">
+
+								<div class="talent">
+									<h2>Programming</h2>
+									<p>.	</p>
+								</div>
+
+								<div class="talent">
+									<h2>Mathematics Logician</h2>
+									<p>.	 </p>
+								</div>
+
+								<div class="talent">
+									<h2>Innovative</h2>
+									<p>.</p>
+								</div>
+						</div>
+					</div><!--// .yui-gf -->
+
+					<div class="yui-gf">
+						<div class="yui-u first">
+							<h2>Technical</h2>
+						</div>
+						<div class="yui-u">
+							<ul class="talent">
+								<li> Programming</li>
+								<li>C++</li>
+								<li class="last">Java</li>
+							</ul>
+
+							<ul class="talent">
+								<li>Database</li>
+								<li>Oracle</li>
+								<li class="last">SQL</li>
+							</ul>
+
+							<ul class="talent">
+								<li>MS Excel</li>
+								<li>MS Word</li>
+								<li class="last">MS Powerpoint</li>
+							</ul>
+						</div>
+					</div><!--// .yui-gf-->
+
+					<div class="yui-gf">
+	
+						<div class="yui-u first">
+							<h2>Experience</h2>
+						</div><!--// .yui-u -->
+
+						<div class="yui-u">
+							<div class="job">
+								<h2>Google Students Developer Club.</h2>
+								<h3>Member</h3>
+								<h4>2021-present</h4>
+								<p>Progressively reconceptualize multifunctional "outside the box" thinking through inexpensive methods of empowerment. Compellingly morph extensive niche markets with mission-critical ideas. Phosfluorescently deliver bricks-and-clicks strategic theme areas rather than scalable benefits. </p>
+							</div>
+							
+							<div class="job">
+								<h2>FEU Alabang ACM</h2>
+								<h3>Director of Membership Officer</h3>
+								<h4>2020-2021</h4>
+								<p>Intrinsicly enable optimal core competencies through corporate relationships. Phosfluorescently implement worldwide vortals and client-focused imperatives. Conveniently initiate virtual paradigms and top-line convergence. </p>
+							</div>
+
+							<div class="job">
+								<h2>National Service Reserve Corps</h2>
+								<h3>Course Representative and Auditor</h3>
+								<h4>2019-2020</h4>
+								<p>Intrinsicly transform flexible manufactured products without excellent intellectual capital. Energistically evisculate orthogonal architectures through covalent action items. Assertively incentivize sticky platforms without synergistic materials. </p>
+							</div>
+
+
+							<div class="job last">
+								<h2>CREOTEC</h2>
+								<h3>Rework Department Trainee</h3>
+								<h4>2018-2019</h4>
+								<p>Globally re-engineer cross-media schemas through viral methods of empowerment. Proactively grow long-term high-impact human capital and highly efficient innovation. Intrinsicly iterate excellent e-tailers with timely e-markets.</p>
+							</div>
+
+						</div><!--// .yui-u -->
+					</div><!--// .yui-gf -->
+
+
+					<div class="yui-gf last">
+						<div class="yui-u first">
+							<h2>Education</h2>
+						</div>
+						<div class="yui-u">
+							<h2>FEU Alabang - Muntinlupa, Philippines</h2>
+							<h3>BS Computer Science Specializing in Software Engineering &mdash; <strong>4.0 GPA</strong> </h3>
+						</div>
+					</div><!--// .yui-gf -->
+
+
+				</div><!--// .yui-b -->
+			</div><!--// yui-main -->
+		</div><!--// bd -->
+
+		<div id="ft">
+			<p>Jose Carlos R. Limpiada &mdash; 201910077@feualabang.edu.ph</a> &mdash; (123) - 456 - 789</p>
+		</div><!--// footer -->
+
+	</div><!-- // inner -->
+
+
+</div><!--// doc -->
+
+
+</body>
+</html>
+
